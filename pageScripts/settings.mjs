10414b773afb8d75a4db4bd60da9f6e3c908d670
p-sw <shinwoo.park@psw.kr>
2024-11-25 08:22:31 +0900

    feat: add review selector option disable event

--- pageScripts/settings.mjs
+++ pageScripts/settings.mjs
@@ -23,35 +23,82 @@ export default class SettingsPage extends BasePage {
     const content = document.createElement("div");
     content.innerHTML = await fetch('/pageScripts/settings.html').then(r => r.text());
 
+    /*** 변수 충돌 관리를 위한 클로저화 ***/
+
     /* 탭 기능 */
-    const tabSelects = content.querySelectorAll(".select-tab");
-    for (const tabSelect of tabSelects) {
-      const tabs = {}
-      for (const tab of tabSelect.querySelectorAll("button")) {
-        const tabId = tab.dataset.tabId;
-        tabs[tabId] = [
-          tab,
-          tabSelect.parentElement.querySelector(`.select-tab ~ .tab-content > div[data-tab-id="${tabId}"]`)
-        ]
+    (() => {
+      const tabSelects = content.querySelectorAll(".select-tab");
+      for (const tabSelect of tabSelects) {
+        const tabs = {}
+        for (const tab of tabSelect.querySelectorAll("button")) {
+          const tabId = tab.dataset.tabId;
+          tabs[tabId] = [
+            tab,
+            tabSelect.parentElement.querySelector(`.select-tab ~ .tab-content > div[data-tab-id="${tabId}"]`)
+          ]
+        }
+        new Tab(tabs);
       }
-      new Tab(tabs);
-    }
+    })();
 
     /* 리뷰 기준 옵션 삽입 */
-    const reviewElements = [];
-    for (const [idx, reviewSummary] of Object.entries(reviewSelections)) {
-      const c = document.createElement("option");
-      c.innerText = reviewSummary;
-      c.value = idx;
-      reviewElements.push(c);
-    }
-
-    const reviewSelectors = content.querySelectorAll(".review-selector")
-    for (const review of reviewSelectors) {
-      for (const element of reviewElements) {
-        review.appendChild(element.cloneNode(true))
+    (() => {
+      const reviewElements = [];
+      for (const [idx, reviewSummary] of Object.entries(reviewSelections)) {
+        const c = document.createElement("option");
+        c.innerText = reviewSummary;
+        c.value = idx;
+        reviewElements.push(c);
       }
-    }
+
+      const reviewSelectors = content.querySelectorAll(".review-selector")
+      for (const review of reviewSelectors) {
+        for (const element of reviewElements) {
+          review.appendChild(element.cloneNode(true))
+        }
+      }
+    })();
+
+    /* 최대/최소 옵션 변경 */
+    (() => {
+      const reviewMin = content.querySelector('select#review_selection_min');
+      const reviewMax = content.querySelector('select#review_selection_max');
+
+      const reviewMinOptions = reviewMin.querySelectorAll("option");
+      const reviewMaxOptions = reviewMax.querySelectorAll("option");
+
+      reviewMin.addEventListener("change", (e) => {
+        const value = parseInt(e.currentTarget.value);
+        const shouldDisabledRange = [value + 1, reviewSelections.length - 1];
+
+        reviewMaxOptions.forEach(
+          (option) =>
+            value === 0
+              ? option.disabled = false
+              : option.value !== "0" &&
+                option.value >= shouldDisabledRange[0] &&
+                option.value <= shouldDisabledRange[1]
+                  ? option.disabled = true
+                  : option.disabled = false
+        );
+      })
+
+      reviewMax.addEventListener("change", (e) => {
+        const value = parseInt(e.currentTarget.value);
+        const shouldDisabledRange = [0, value - 1];
+
+        reviewMinOptions.forEach(
+          (option) =>
+            value === 0
+              ? option.disabled = false
+              : option.value !== "0" &&
+                option.value >= shouldDisabledRange[0] &&
+                option.value <= shouldDisabledRange[1]
+                  ? option.disabled = true
+                  : option.disabled = false
+        );
+      })
+    })();
 
     return content;
   }
