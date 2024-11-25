a028872e9b5adb078cf66c349680fe016b1eb0ce
p-sw <shinwoo.park@psw.kr>
2024-11-25 15:40:47 +0900

    fix: remove intentional loading time for testing

--- pageScripts/history.mjs
+++ pageScripts/history.mjs
@@ -6,9 +6,6 @@ export default class HistoryPage extends BasePage {
   }
 
   async mountContent() {
-    await new Promise((r) => {
-      setTimeout(r, 10000);
-    });
     const content = document.createElement("div");
     content.innerHTML = await fetch('/pageScripts/history.html').then(r => r.text());
     return content;
